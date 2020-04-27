ef67521784d34336fad61f0e7788cbfbde9ad255
Anisa <aagermanova@gmail.com>
2020-04-27 16:01:13 -0700

    Создаст контроллер точки маршрута

---
+++ src/controllers/point.js
@@ -0,0 +1,9 @@
+import EventItem from "../components/event-item.js";
+import EditEvent from "../components/edit-event.js";
+import {renderElement, RenderPosition, replace, remove} from "../utils/render.js";
+
+export default class PointController {
+  constructor(container) {}
+
+  render(event) {}
+}
